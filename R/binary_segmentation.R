b0032fb2863367a6f175317287fb6d96d4de7042
Lorenz Haubner <lorenz.haubner@gmail.com>
2017-11-28 10:16:44 +0100

    some refactoring and documentation update

--- R/binary_segmentation.R
+++ R/binary_segmentation.R
@@ -5,15 +5,15 @@
 #' a changepoint estimate.
 #'
 #' @inheritParams TernarySearch
-#' @param x A n times p matrix for which to find the best splitting point.
-#' @param delta Value between 0 and 1. Tuning param which determines the minimal segment size
+#' @param x A n times p data matrix.
+#' @param delta Value between 0 and 0.5. Tuning param which determines the minimal segment size
 #' proportional to the size of the dataset and hence an upper bound for the number of changepoints.
 #' @param lambda Sparsity penality parameter in single lasso fits.
 #' @param method Which method should be used for fitting the model? See defaults for possible choices.
 #' @param penalize_diagonal Boolean, should the diagonal elements of the precision matrix be penalized?
 #' @param threshold The threshold for halting the iteration in glasso or glmnet. In the former it controls the change of single parameters
 #' in the latter it controls the total objective value.
-#' @param use_ternary_search Use a ternary search algorithm in each level of the recursion to find a local optimum (EXPERIMNETAL)
+#' @param use_ternary_search Use ternary search in each level of the recursion to find a local optimum (EXPERIMENTAL).
 #'
 #' @return An object of class \strong{bs_tree}
 #' @export
@@ -23,14 +23,15 @@
 #' res <- BinarySegmentation(dat, delta = 0.1, lambda = 0.01, method = "summed_regression")
 #' print(res)
 BinarySegmentation <- function(x, delta, lambda,
-                               method = c("glasso", "nodewise_regression", "summed_regression", "ratio_regression"),
+                               method = c("nodewise_regression", "summed_regression", "ratio_regression"),
                                threshold = 1e-7,
                                penalize_diagonal = F,
                                use_ternary_search = F,
                                intervals = 3,
                                ...) {
-  meth <- match.arg(method)
-  SegmentLossFUN <- SegmentLoss(n_obs = nrow(x), lambda = lambda, penalize_diagonal = penalize_diagonal, method = meth, threshold = threshold, ...)
+  mth <- match.arg(method)
+  SegmentLossFUN <- SegmentLoss(n_obs = nrow(x), lambda = lambda, penalize_diagonal = penalize_diagonal,
+                                method = mth, threshold = threshold, ...)
 
   tree <- data.tree::Node$new("bs_tree", start = 1, end = nrow(x))
 
@@ -44,7 +45,6 @@ BinarySegmentation <- function(x, delta, lambda,
       node$min_loss <- min(res[["loss"]])
       node$loss     <- res[["loss"]]
       node$segment_loss <- res[["segment_loss"]]
-
       split_point <- res[["opt_split"]]
 
       if (is.na(split_point)) {
@@ -52,11 +52,15 @@ BinarySegmentation <- function(x, delta, lambda,
       } else {
         start <- node$start
 
-        child_left <- node$AddChild(as.character(start), start = start, end = start + split_point - 1)
-        alpha_left <- Rec(x[1:(split_point - 1),, drop = F], n_obs, delta, SegmentLossFUN, child_left, use_ternary_search, intervals)
+        child_left <- node$AddChild(as.character(start), start = start,
+                                    end = start + split_point - 1)
+        Rec(x[1:(split_point - 1),, drop = F], n_obs, delta,
+            SegmentLossFUN, child_left, use_ternary_search, intervals)
 
-        child_right <- node$AddChild(as.character(start + split_point - 1), start = start + split_point - 1, end = start + n_selected_obs -1)
-        alpha_right <- Rec(x[split_point:n_selected_obs,, drop = F], n_obs, delta, SegmentLossFUN, child_right, use_ternary_search, intervals)
+        child_right <- node$AddChild(as.character(start + split_point - 1), start = start + split_point - 1,
+                                     end = start + n_selected_obs - 1)
+        Rec(x[split_point:n_selected_obs,, drop = F], n_obs, delta,
+            SegmentLossFUN, child_right, use_ternary_search, intervals)
       }
     } else {
       return(NA)
@@ -70,12 +74,13 @@ BinarySegmentation <- function(x, delta, lambda,
 
 #' FindBestSplit
 #'
-#' Uses the segment_loss function for each possible split into two segements and
-#' returns the best splitting index as well as the loss for each candidate.
+#' Uses the SegmentLossFUN function for each possible split on the given segment
+#' considering delta and returns the splitting index with the lowest loss
+#' as well as the loss for each candidate.
 #'
 #' @inheritParams BinarySegmentation
 #' @inheritParams TernarySearch
-#' @param SegmentLossFUN A loss function is created by \code{\link{SegmentLoss}}
+#' @param SegmentLossFUN A loss function as created by \code{\link{SegmentLoss}}
 FindBestSplit <- function(x, delta, n_obs, use_ternary_search, SegmentLossFUN, intervals) {
   obs_count <- nrow(x)
   min_seg_length <- ceiling(delta * n_obs)
@@ -93,33 +98,30 @@ FindBestSplit <- function(x, delta, n_obs, use_ternary_search, SegmentLossFUN, i
   if (use_ternary_search) {
     result <- TernarySearch(split_candidates, 1, length(split_candidates), x, SegmentLossFUN, intervals)
   } else {
-    loss <- numeric(length(split_candidates))
-    for (i in seq_along(split_candidates)) {
-      loss[i] <- SplitLoss(x, split_point = split_candidates[i], SegmentLossFUN)
-    }
+    loss <- sapply(split_candidates, function(y) SplitLoss(x, y, SegmentLossFUN = SegmentLossFUN))
     result <- list(opt_split = split_candidates[which.min(loss)], loss = loss)
   }
 
   if (round(min(result[["loss"]]), 15) >= round(segment_loss, 15))
     list(opt_split = NA, loss = result[["loss"]], segment_loss = segment_loss)
     else
-    list(opt_split = result[["opt_split"]], loss = result[["loss"]], segment_loss = segment_loss) # Add one since we want to split so that the minimum stays in the left segment (design choice)
+    list(opt_split = result[["opt_split"]], loss = result[["loss"]], segment_loss = segment_loss)
 }
 
 #' TernarySearch
 #'
 #' Use a ternary search technique to find a local minimum for the next split recursively.
 #'
-#' Besides the standard ternary approach where the segment is divided into three intervals adn evaluated at two points one
-#' can also define a more granular intervals until intervals = number of observations in which case all observations will
-#' be evaluated and we are in the case of searching for the minium exactly.
+#' Besides the standard ternary approach where the segment is divided into three intervals and evaluated at two points one
+#' can also define a more granular grid until the number of intervals is equal to the number
+#' of observations in which case all observations will be evaluated and we are in the case of searching for the minimum exactly.
 #'
-#' @param split_candidates A list of indices of possible split points
+#' @param split_candidates A vector of indices of possible split points
 #' @param x An n times p data matrix
 #' @param left start index on the left
 #' @param right start index on the right
-#' @param SegmentLossFUN A loss function is created by \code{\link{SegmentLoss}}
-#' @param intervals Number of intervals of the search grid
+#' @param SegmentLossFUN A loss function as created by \code{\link{SegmentLoss}}
+#' @param intervals Number of intervals of the search grid. Only used by ternary search.
 TernarySearch <- function(split_candidates, left, right, x, SegmentLossFUN, intervals = 3) {
 
   # Stopping condition for recursion
