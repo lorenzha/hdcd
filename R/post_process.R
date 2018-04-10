94d5730e96de034d468ce69f9ec72afd87fc7582
Lorenz Haubner <lorenz.haubner@gmail.com>
2018-04-10 14:38:08 +0200

    add function for postprocessing the found changepoints

---
+++ R/post_process.R
@@ -0,0 +1,32 @@
+
+
+
+#' PostProcess
+#'
+#' Adjust the position of each changepoint by refitting the model in the respective segments.
+#'
+#' @inheritParams BinarySegmentation
+#' @param cpts A numeric vector containing the found changepoints. Can be of length zero if no changepoints have been found.
+#'
+#' @return A nuemric vector containing the final segment boundaries
+#' @export
+#'
+PostProcess <- function(x, cpts, delta, lambda, method){
+  n_obs <- nrow(x)
+
+  seg_boundaries <- c(1, cpts, n_obs)
+
+  if (length(seg_boundaries) < 3)
+    return(seg_boundaries)
+  else{
+    for (i in seq(2,length(seg_boundaries)-1)){
+      seg_inds <- seg_boundaries[i-1]:(seg_boundaries[i+1]-1)
+
+      opt_split_ind <- hdcd:::FindBestSplit(x[seg_inds,], delta = delta, n_obs = n_obs, optimizer = "line_search", control = NULL,
+                                            SegmentLossFUN =  hdcd:::SegmentLoss(n_obs = n_obs, lambda = lambda, method = method))$opt_split
+
+      seg_boundaries[i] <- seg_inds[opt_split_ind]
+    }
+  }
+  seg_boundaries
+}
