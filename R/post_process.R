ff8e9d5b032a488ab8870a55c3ae81e66c9f23c9
Lorenz Haubner <lorenz.haubner@gmail.com>
2018-07-13 12:06:13 +0200

    remove unnecessary ':::'

--- R/post_process.R
+++ R/post_process.R
@@ -20,8 +20,8 @@ PostProcess <- function(x, cpts, delta, lambda, method){
     for (i in seq(2,length(seg_boundaries)-1)){
       seg_inds <- seg_boundaries[i-1]:(seg_boundaries[i+1]-1)
 
-      opt_split_ind <- hdcd:::FindBestSplit(x[seg_inds,], delta = delta, n_obs = n_obs, optimizer = "line_search", control = NULL,
-                                            SegmentLossFUN =  hdcd:::SegmentLoss(n_obs = n_obs, lambda = lambda, method = method))$opt_split
+      opt_split_ind <- FindBestSplit(x[seg_inds,], delta = delta, n_obs = n_obs, optimizer = "line_search", control = NULL,
+                                            SegmentLossFUN =  SegmentLoss(n_obs = n_obs, lambda = lambda, method = method))$opt_split
 
       seg_boundaries[i] <- if (is.na(opt_split_ind)) NA else seg_inds[opt_split_ind]
     }
