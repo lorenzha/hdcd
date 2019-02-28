9ac69ed5c79a5f04fe7196a5cf7ba762d9e51868
Malte Londschien <maltelondschien@gmail.com>
2019-02-28 13:12:46 +0100

    simplified loglikelihood function

--- R/split_function.R
+++ R/split_function.R
@@ -105,55 +105,82 @@ make_glasso_fit <- function(x, lambda, n_obs, NA_method, control){
 # calculates loglikelihood of x (with missing values) given mu, cov_mat. cov_mat_inv can be used to speed up calculaitons
 # of the inverse of the submatrix via the Woodbury matrix identity as a low rank update from cov_mat_inv
 # Maybe improve speed??
+# loglikelihood <- function(x, mu, cov_mat, cov_mat_inv, standardize_loglik = F){
+#   if(!is.matrix(x)){
+#     x <- as.matrix(x)
+#     warning('non matrix input into loglikelihood')
+#   }
+#   p <- ncol(x)
+#   inds <- is.na(x) # missingness structure of x
+#   na_order <- do.call(order, lapply(1:ncol(inds), function(i) inds[, i])) # order x by missingness structure
+#   cov_mat_inv_cur <- cov_mat_inv
+#   log_det_cur <- determinant(cov_mat, logarithm = TRUE)$modulus
+#   attributes(log_det_cur) <- NULL
+#   loss <- 0
+#
+#   inds_old <- rep(F, ncol(x))
+#   k <- 0
+#   for(i in 1:nrow(x)){
+#     inds_cur <- inds[na_order[i], ]
+#     if(any(inds_cur != inds_old)){
+#       inds_old <- inds_cur
+#       if (sum(inds_cur) < length(inds_cur) / 3){
+#         # fast calculation of cov_mat_inv_cur as update from cov_mat_inv via Woodbury matrix identity
+#         k <- sum(inds[na_order[i], ]) # amount of missing values
+#         A <- diag(p)[, inds_cur, drop = F] # helper matrix
+#         U <- cbind(A, cov_mat[, inds_cur])
+#         U[, (k + 1) : (2*k)][as.logical(A)] <- 0
+#         V <- rbind(cov_mat[inds_cur, ], t(A))
+#         V[1 : k, ][as.logical(t(A))] <- 0
+#         V[1 : k, ][as.logical(t(A[, k : 1]))] <- 0
+#         S <- -diag(2 * k) + V %*% cov_mat_inv %*% U
+#         cov_mat_inv_cur <- tryCatch({
+#           #solve(cov_mat[!inds_cur, !inds_cur])
+#           (cov_mat_inv - cov_mat_inv %*% U %*% solve(S) %*% V %*% cov_mat_inv)[!inds_cur, !inds_cur]
+#         }, condition = function(e){
+#           #warning('loglikelihood was not able to apply the Woodbury matrix identity')
+#           solve(cov_mat[!inds_cur, !inds_cur])
+#         })
+#       } else {
+#         cov_mat_inv_cur <- solve(cov_mat[!inds_cur, !inds_cur])
+#       }
+#       log_det_cur <- determinant(cov_mat[!inds_cur, !inds_cur], logarithm = TRUE)$modulus
+#       attributes(log_det_cur) <- NULL
+#     }
+#     v <- (x[na_order[i], ] - mu)[!inds_cur]
+#     if (standardize_loglik){
+#       loss <- loss + (t(v) %*% cov_mat_inv_cur %*% v - p) / sqrt(p / (p - k))
+#     } else {
+#       loss <- loss + t(v) %*% cov_mat_inv_cur %*% v + log_det_cur #+ (p - k) * log(2*pi)  #This p+k is essential. WHY??
+#       #cat(i, ', addition to loss = ',  t(v) %*% cov_mat_inv_cur %*% v, ' + ', log_det_cur, '\n')
+#     }
+#   }
+#   loss / 2
+# }
+
+
 loglikelihood <- function(x, mu, cov_mat, cov_mat_inv, standardize_loglik = F){
   if(!is.matrix(x)){
     x <- as.matrix(x)
     warning('non matrix input into loglikelihood')
   }
+  n <- nrow(x)
   p <- ncol(x)
-  inds <- is.na(x) # missingness structure of x
-  na_order <- do.call(order, lapply(1:ncol(inds), function(i) inds[, i])) # order x by missingness structure
-  cov_mat_inv_cur <- cov_mat_inv
-  log_det_cur <- determinant(cov_mat, logarithm = TRUE)$modulus
-  attributes(log_det_cur) <- NULL
-  loss <- 0
+  loss <- numeric(n)
 
-  inds_old <- rep(F, ncol(x))
-  k <- 0
-  for(i in 1:nrow(x)){
-    inds_cur <- inds[na_order[i], ]
-    if(any(inds_cur != inds_old)){
-      inds_old <- inds_cur
-      if (sum(inds_cur) < length(inds_cur) / 3){
-        # fast calculation of cov_mat_inv_cur as update from cov_mat_inv via Woodbury matrix identity
-        k <- sum(inds[na_order[i], ]) # amount of missing values
-        A <- diag(p)[, inds_cur, drop = F] # helper matrix
-        U <- cbind(A, cov_mat[, inds_cur])
-        U[, (k + 1) : (2*k)][as.logical(A)] <- 0
-        V <- rbind(cov_mat[inds_cur, ], t(A))
-        V[1 : k, ][as.logical(t(A))] <- 0
-        V[1 : k, ][as.logical(t(A[, k : 1]))] <- 0
-        S <- -diag(2 * k) + V %*% cov_mat_inv %*% U
-        cov_mat_inv_cur <- tryCatch({
-          #solve(cov_mat[!inds_cur, !inds_cur])
-          (cov_mat_inv - cov_mat_inv %*% U %*% solve(S) %*% V %*% cov_mat_inv)[!inds_cur, !inds_cur]
-        }, condition = function(e){
-          #warning('loglikelihood was not able to apply the Woodbury matrix identity')
-          solve(cov_mat[!inds_cur, !inds_cur])
-        })
-      } else {
-        cov_mat_inv_cur <- solve(cov_mat[!inds_cur, !inds_cur])
-      }
-      log_det_cur <- determinant(cov_mat[!inds_cur, !inds_cur], logarithm = TRUE)$modulus
-      attributes(log_det_cur) <- NULL
-    }
-    v <- (x[na_order[i], ] - mu)[!inds_cur]
-    if (standardize_loglik){
-      loss <- loss + (t(v) %*% cov_mat_inv_cur %*% v - p) / sqrt(p / (p - k))
+  for (i in 1 : nrow(x)){
+    inds <- !is.na(x[i, ])
+    if(any(!inds)){
+      log_det <- determinant(cov_mat[inds, inds], logarithm = TRUE)$modulus
+      v <- x[i, inds] - mu[inds]
+      distance <- t(v) %*% solve(cov_mat[inds, inds], v) ##vectorize, b = matrix, for rows of same missingness structure
+      loss[i] <- distance + log_det + sum(inds) * log(2*pi)
     } else {
-      loss <- loss + t(v) %*% cov_mat_inv_cur %*% v + log_det_cur #+ (p - k) * log(2*pi)  #This p+k is essential. WHY??
-      #cat(i, ', addition to loss = ',  t(v) %*% cov_mat_inv_cur %*% v, ' + ', log_det_cur, '\n')
+      v <- x - mu
+      distance <- t(v) %*% cov_mat_inv %*% v
+      loss[i] <- distance + log_det_full + p * log(2*pi)
     }
   }
+
   loss / 2
 }
